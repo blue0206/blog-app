546826d965e74060d0a94f87da1ae5cc6f6dd584
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-01-15 22:45:10 +0530

    Refactor configuration structure to use TypeScript types for better type safety

--- src/conf/conf.ts
+++ src/conf/conf.ts
@@ -1,4 +1,12 @@
-const conf = {
+type Conf = {
+    appwriteUrl: string;
+    appwriteProjectId: string;
+    appwriteDatabaseId: string;
+    appwriteCollectionId: string;
+    appwriteBucketId: string;
+};
+
+const conf: Conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL!),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID!),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID!),
