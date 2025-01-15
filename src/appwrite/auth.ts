82d7bcd6efae73b7ee2839233b196919cfb99ccd
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-01-15 23:52:25 +0530

    Implement AuthService for user account management with Appwrite

---
+++ src/appwrite/auth.ts
@@ -0,0 +1,63 @@
+import conf from "../conf/conf.ts";
+import { Client, Account, ID } from "appwrite";
+import {createAccount, emailLogin} from "../../types/auth-service-types.ts";
+
+export class AuthService {
+    client: Client = new Client();
+    account: Account;
+
+    constructor() {
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({ email, password, name }: createAccount): Promise<unknown> {
+        try {
+            const userAccount = await this.account.create(
+                ID.unique(),
+                email,
+                password,
+                name
+            );
+            if (!userAccount) {
+                throw new Error("Failed to create account.");
+            }
+            return this.login({ email, password });
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async login({ email, password }: emailLogin): Promise<unknown> {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite Service :: getCurrentUser :: Error", error);
+        }
+        return null;
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite Service :: Logout :: Error", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
